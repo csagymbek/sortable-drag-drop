1b3a6083215c2de0a77bd1dc29878b67f6f21cc7
Serik <csagymbek@gmail.com>
2021-08-27 21:33:38 -0700

    js logic completed

--- app.js
+++ app.js
@@ -1,8 +1,44 @@
-const drags = document.querySelectorAll("p");
-const containers = document.querySelectorAll("div");
+const containers = document.querySelectorAll(".container");
+const draggables = document.querySelectorAll(".draggable");
 
-Array.from(drags).forEach((drag) => {
-  drag.addEventListener("dragstart", () => {
-    console.log("dragstart");
+draggables.forEach((draggable) => {
+  draggable.addEventListener("dragstart", () => {
+    draggable.classList.add("dragging");
+  });
+  draggable.addEventListener("dragend", () => {
+    draggable.classList.remove("dragging");
   });
 });
+
+containers.forEach((container) => {
+  container.addEventListener("dragover", (e) => {
+    e.preventDefault();
+    const dragging = document.querySelector(".dragging");
+    const afterEl = getDragAfterElement(container, e.clientY);
+    if (afterEl === null) {
+      container.appendChild(dragging);
+    } else {
+      container.insertBefore(dragging, afterEl);
+    }
+  });
+});
+
+const getDragAfterElement = (container, y) => {
+  const draggableElements = Array.from(
+    container.querySelectorAll(".draggable:not(.dragging)")
+  );
+  return draggableElements.reduce(
+    (closest, child) => {
+      const box = child.getBoundingClientRect();
+      const offset = y - box.top - box.height / 2;
+      if (offset < 0 && offset > closest.offset) {
+        return { offset, element: child };
+      } else {
+        return closest;
+      }
+    },
+    {
+      offset: Number.NEGATIVE_INFINITY,
+    }
+  ).element;
+};
